97ce05fde77f45d8b23c214af28f6bd16cd1dfff
Pablo Alexis Dominguez Grau <pabloadominguezgrau@gmail.com>
2021-01-24 17:33:01 -0500

    Re-arrange app. Add basic home/about landing page. Add URL configuration.

--- vida_foundation/vida/views.py
+++ vida_foundation/vida/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'vida/home.html')
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, 'vida/about.html')
